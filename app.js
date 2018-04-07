cee8ca539743e58d64c7dcbc6e360d0c3b79f794
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-04-07 14:05:09 +0000

    Bæta við DELETE í CORS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
